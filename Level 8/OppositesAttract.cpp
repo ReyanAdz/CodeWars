44c2ec40c50693ba8648a94c6853b15ec92561c7
ReyanAdz <raa104@sfu.ca>
2025-08-14 14:33:32 -0700

    Added Solution for Opposites Attract problem

---
+++ Level 8/OppositesAttract.cpp
@@ -0,0 +1,29 @@
+/*
+Description of Problem:
+
+Timmy & Sarah think they are in love, but around where they live, 
+they will only know once they pick a flower each. If one of the flowers 
+has an even number of petals and the other has an odd number of petals 
+it means they are in love.
+
+Write a function that will take the number of petals of each flower and 
+return true if they are in love and false if they aren't.
+*/
+
+#include <iostream>
+using namespace std;
+bool lovefunc(int f1, int f2) {
+  if(f1 % 2 == 0 && f2 % 2 != 0 ||f1 % 2 != 0 && f2 % 2 == 0)
+  {
+     return true; 
+  }
+  else
+  {
+     return false;
+  }
+  return true;
+}
+int main()
+{
+    std::cout << lovefunc(4,7) << "\n";
+}
